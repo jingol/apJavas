116f116f1a56728b860dde65f79b3f90069d1d83
Student6 <Student6@10.8.33.106>
2016-09-22 12:33:47 -0400

    moar stirns

--- Introduction/src/DataTypes/StringPractice.java
+++ Introduction/src/DataTypes/StringPractice.java
@@ -5,8 +5,54 @@ import java.util.Scanner;
 public class StringPractice {
 
 	static Scanner input;
+	static String user;
+	static int lineCount;
+	
 	public static void main(String[] args) {
 //		String text = new String("Hello World!");
+		createAScanner();
+		lineCount = 0;
+		demonstrateStringMethods();
+		promptName();
+		talkForever();
+	}
+	
+	private static void promptName() {
+		print("Hello, human! I am a board covered with "
+				+ "semi-conductors \nand other"
+				+ " such electronic components.\n"
+				+ "What is your name?");
+		user = input.nextLine();
+		print("Awesome! I will call you " + user 
+				+ " until you terminate me.");
+		
+		
+	}
+
+	public static void talkForever(){
+		while(true){
+			promptInput();
+		}
+	}
+		
+	
+	public static void createAScanner(){
+		input = new Scanner(System.in);
+	}
+	
+	public static void promptInput(){
+		print(user + " ,try inputting a string!");
+		String userInput = input.nextLine();
+		print("You typed: " + userInput);
+	}
+	
+	public static void print(String s){
+		System.out.println("Line #" + lineCount + ": " + s);
+		lineCount++;
+		
+	}
+	
+	public static void demonstrateStringMethods(){
 		String text1 = "Hello World"; //same as above,shortcut
 		String text2 = "Hello ";
 		String text3 = "World";
@@ -24,8 +70,8 @@ public class StringPractice {
 		if(word1.compareTo(word2) < 0){
 			System.out.println("Word1 is before word2," + 
 					"lexicongraphically.");
-		}
-
+	}
+	
 	}
 
 }
