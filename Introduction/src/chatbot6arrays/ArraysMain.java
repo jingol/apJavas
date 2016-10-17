ba8bbba95c7015d507960460b0259eb92b9ceaa8
Student6 <Student6@10.8.33.106>
2016-10-17 13:17:54 -0400

    arrayssss

--- Introduction/src/chatbot6arrays/ArraysMain.java
+++ Introduction/src/chatbot6arrays/ArraysMain.java
@@ -1,23 +1,99 @@
 package chatbot6arrays;
 
 public class ArraysMain {
+	
+	static int[] exerciseArray = new int[50]; 
+	static String[] deck = new String[52];
 
 	public static void main(String[] args) {
 		//this is how you time how quickly a computer processes
 		long startTime = System.currentTimeMillis();
-		String[] someStrings = new String[100];
-		populateArray(someStrings);
 		
-		changeString(someStrings[99]);
+		SampleElement sample = new SampleElement(10);
+		sample.increase();
+		System.out.println("The sample element has a number equal to " + sample.getNumber());
 		
-		printArray(someStrings);
 		long endTime = System.currentTimeMillis();
 		System.out.println("completed method in " + (endTime-startTime + " milliseconds"));
 		
+		cardList();
+	}
+	
+	public static void popArray(){
+		for(int i = 1; i < 50; i++){
+			exerciseArray[i] = i + 1;
+			System.out.println(exerciseArray[i]);
+		}
+		
+	}
+	
+	public static void randomPopArray(){
+		for (int i = 0; i < exerciseArray.length; i++){
+			double rand = Math.random() * 50;
+			exerciseArray[i] = (int) rand;
+			System.out.println(exerciseArray[i]);
+		}
+		
+	}
+	
+	public static void rollPopulate(){
+		for (int i = 0; i < exerciseArray.length; i++){
+			double roll1 = Math.random() * 6 + 1;
+			double roll2 = Math.random() * 6 + 1;
+			
+			exerciseArray[i] = (int)(roll1 + roll2);
+			System.out.println(exerciseArray[i]);
+		}
+	}
+	
+	public static void cardList(){
+		String[] suits = {"Diamond","Clubs","Heart","Spade"};
+		String[] rank = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen"
+				,"King"};
+		int cardIndex = 0;
+		for(int i = 0; i < rank.length; i++){
+			for(int s = 0; i < suits.length; i++){
+				deck[cardIndex + 1] = rank[i] + "of" + suits[s];
+				cardIndex++;
+			}
+			
+		}
+		
+		printArray(deck);
+	}
+	
+	private static void passByValueDemonstration(){
+		String[] someStrings = new String[100];
+		populateArray(someStrings);
+		
+		//no change
+		changeString(someStrings[99]);
+		//no change
+		changeArray(someStrings);
+		//yes change
+		changeArrayElement(someStrings, 99);
+		System.out.println(someStrings[99]);
 	}
 	
 	
 	
+	private static void changeArrayElement(String[] someStrings, int i) {
+		someStrings[i] = "new item " + (i+1);
+		
+	}
+
+
+
+	private static void changeArray(String[] someStrings) {
+		someStrings = new String[100];
+		for(int i = 0; i < someStrings.length; i++){
+			someStrings[i] = "new item "+ (i+1);
+		}
+		
+	}
+
+
+
 	private static void changeString(String s){
 		s = "This string has been changed";
 	}
@@ -30,7 +106,6 @@ public class ArraysMain {
 	}
 
 	private static void populateArray(String[] a) {
-		
 		
 		for (int index = 0; index < a.length; index++){
 			a[index] = "value  " + (index + 1);
---
+++ Introduction/src/chatbot6arrays/SampleElement.java
@@ -0,0 +1,20 @@
+package chatbot6arrays;
+
+public class SampleElement {
+
+	//field
+	private int number;
+	
+	//constructor
+	public SampleElement(int value){
+		number = value;
+	}
+	
+	public int getNumber(){
+		return number;
+	}
+	
+	public void increase(){
+		number++;
+	}
+}
