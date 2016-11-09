fb8d0cd32858fe5f7a4a4328df176157a0644634
Student6 <Student6@10.8.33.106>
2016-11-09 12:33:21 -0500

    pomf

--- Introduction/src/caveExplorer/Inventory.java
+++ Introduction/src/caveExplorer/Inventory.java
@@ -10,8 +10,46 @@ public class Inventory {
 		updateMap();
 	}
 	
-	private void updateMap() {
+	public void updateMap() {
+		map = " ";
+		for(int i = 0; i < CaveExplorer.caves[0].length - 1; i++){
+			map += "____"; //4 underscores
+		}
+		
+		map += "___\n";//3 underscores
+		
+		for(CaveRoom[] row: CaveExplorer.caves){
+			//three rows of text
+			
+			for(int i = 0; i < 3; i++){
+				//a line of text for each row
+				String text = " ";
+				for(CaveRoom cr: row){
+					 text += "|";
+					if(cr.getDoor(CaveRoom.WEST) != null 
+							&& cr.getDoor(CaveRoom.WEST).isOpen()){
+						text += " ";				
+					}
+					if(i == 0){
+						text += "   "; // 3 spaces
+					}else if(i == 1){
+						text += " " + cr.getContents()+ " ";
+					}
+					else if(i == 2){
+						if(cr.getDoor(CaveRoom.SOUTH) != null 
+								&& cr.getDoor(CaveRoom.SOUTH).isOpen()){
+							text += "   "; // 3 spaces
+						}
+						else{
+							text += "___"; // 3 underscores
+						}
+					}
+				}//last caveroom in the row
 				
+				text += "|";
+				map += text + "\n";
+			}//last of the 3 text lines
+		}//last row
 	}
 
 	public String getDescription() {
