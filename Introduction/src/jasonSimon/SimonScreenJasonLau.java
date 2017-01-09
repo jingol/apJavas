ebded38a862b7045b75d0411714039b7bff6a075
Student6 <Student6@10.8.33.97>
2017-01-09 12:32:16 -0500

    lel

--- Introduction/src/jasonSimon/SimonScreenJasonLau.java
+++ Introduction/src/jasonSimon/SimonScreenJasonLau.java
@@ -3,6 +3,7 @@ package jasonSimon;
 import java.util.ArrayList;
 import java.util.List;
 
+import guiCompononets.ClickableGraphic;
 import guiCompononets.TextLabel;
 import guiCompononets.Visible;
 import guiScreens.ClickableScreen;
@@ -14,23 +15,28 @@ public class SimonScreenJasonLau extends ClickableScreen implements Runnable {
 	public ProgressInterfaceJasonLau prog;
 	public ArrayList<MoveInterfaceJasonLau> seq;
 	public int round;
+	public int seqIndex;
 	public boolean inputAccepted;
+	public TextLabel text;
+	public int lastSelectedButton;
+	public ButtonInterfaceJasonLau[] butts;
 
 
 	public SimonScreenJasonLau(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
+		Thread sequence = new Thread(this);
+		text = new TextLabel(50,20,40,40,"It's Simooooon");
+		
 
 	}
 
@@ -39,5 +45,16 @@ public class SimonScreenJasonLau extends ClickableScreen implements Runnable {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public ButtonInterfaceJasonLau getAButton(){
+		return null;
+	}
+	
+	public ProgressInterfaceJasonLau getProgress(){
+		return null;
+		
+	}
+	
+	
 
 }
